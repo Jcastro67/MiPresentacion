df0327cf0d16aa8f7a2e691fea6971321561c641
Jcastro <jcastro0067@gmail.com>
2016-10-19 18:49:32 -0600

    una sola actividad

---
+++ app/src/main/java/com/castro/presentacion/MainActivity.java
@@ -0,0 +1,28 @@
+package com.castro.presentacion;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button entrar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        entrar = (Button)findViewById(R.id.btnIntro);
+        entrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent entrar = new Intent (MainActivity.this,presentacion.class);
+                startActivity(entrar);
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/castro/presentacion/presentacion.java
@@ -0,0 +1,16 @@
+package com.castro.presentacion;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Administrador on 18/10/2016.
+ */
+public class presentacion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act2);
+    }
+
+}
